992e3e1029282d21008c901a1384cc798bd61a39
Joao Paulo S Bembe <joaobembe@icloud.com>
2023-07-17 16:07:31 -0300

    Primeiros passos com java e Spring Boot

---
+++ src/test/java/com/joaobembe/aprendizadospringbootrestapi/StartupTests.java
@@ -0,0 +1,13 @@
+package com.joaobembe.aprendizadospringbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
