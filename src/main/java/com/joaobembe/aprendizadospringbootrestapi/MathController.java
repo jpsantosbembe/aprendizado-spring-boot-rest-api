d4b27ab3b5d3d3bd5ee2e71f9ec3bb6b164789b3
Joao Paulo S Bembe <joaobembe@icloud.com>
2023-07-18 16:16:58 -0300

    Implementado tratamento de exceção para valores nao numericos

--- src/main/java/com/joaobembe/aprendizadospringbootrestapi/MathController.java
+++ src/main/java/com/joaobembe/aprendizadospringbootrestapi/MathController.java
@@ -1,5 +1,8 @@
 package com.joaobembe.aprendizadospringbootrestapi;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.joaobembe.aprendizadospringbootrestapi.exceptions.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,7 +11,22 @@ public class MathController {
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
-    ) {
-        return 1D;
+    ) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Valor não é numérico");
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(strNumber)) return Double.parseDouble(number);
+        return 0d;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",",".");
+        return number.matches("[+-]?[0-9]*\\.?[0-9]+");
     }
 }
